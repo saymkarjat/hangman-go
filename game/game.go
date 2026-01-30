d9be348236d87cd0bbc6f41aed0d02bf20232863
Mikhail Popkov <mpopkov@ibsys.ru>
2026-01-30 17:31:46 +0300

    Implement game initialization with random word selection and difficulty input

--- game/game.go
+++ game/game.go
@@ -1,4 +1,46 @@
 package game
 
+import (
+	"fmt"
+	"hangman-go/words"
+)
+
 type Game struct {
+	playerName string
+	isOver     bool
+}
+
+func New(playerName string) *Game {
+	return &Game{
+		playerName: playerName,
+	}
+}
+
+func (g *Game) Play(playerName string) {
+	for !g.isOver {
+		g.startGame(playerName)
+	}
+}
+
+func (g *Game) startGame(playerName string) {
+	fmt.Printf("Привет уважаемый " + playerName + ", введи сложность игры(easy, medium, hard): ")
+	randomWord := g.chooseDifficulty().RandomWord()
+	fmt.Println(randomWord)
+}
+
+func (g *Game) chooseDifficulty() words.WordList {
+	for {
+		difficulty := inputString()
+		wordList, err := words.GetWordList(words.Difficulty(difficulty))
+		if err == nil {
+			return wordList
+		}
+		fmt.Printf("ошибка, введите сложность игры(easy, medium, hard): ")
+	}
+}
+
+func inputString() string {
+	var input string
+	_, _ = fmt.Scanln(&input)
+	return input
 }
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"hangman-go/game"
+)
 
+func main() {
+	fmt.Printf("введите ваше имя: ")
+	var playerName string
+	_, _ = fmt.Scanln(&playerName)
+
+	g := game.New(playerName)
+	g.Play(playerName)
 }
